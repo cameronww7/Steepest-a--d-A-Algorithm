ce0eb9796a4151e1e4cf77c12bb93ccec90cb877
cam <cam@localhost.localdomain>
2017-06-08 14:05:43 -0700

    Updated Names

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 /******************************************************************
- * Name			:	Cameron Walters
+ * Name	1		:	Cameron Walters
+ * Name 2		:
+ * Name 3		:
+ * Name 4		:
+ * Name 5		:
  * Project Name	:	Assignment 1
  * Due Date		:	6/20
  * Objectives	: Implementing a steepest-ascent/-descent hill-
