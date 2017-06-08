d8b1b730bef4112f8c920e929846c873687a8f2e
cam <cam@localhost.localdomain>
2017-06-08 15:33:27 -0700

    Added group member's names

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /******************************************************************
  * Name	1		:	Cameron Walters
- * Name 2		:
- * Name 3		:
- * Name 4		:
- * Name 5		:
+ * Name 2		:	Payaam Emami
+ * Name 3		:	Itsarapong Sawangsri
+ * Name 4		:	Justin Aebiskiver
+ * Name 5		:	Kenny Nham
  * Project Name	:	Assignment 1
  * Due Date		:	6/20
  * Objectives	: Implementing a steepest-ascent/-descent hill-
