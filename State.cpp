66a708a3965e4df565c5279c8c40bec89a4778e1
oatItsaraphong <itsaraphong@csu.fullerton.edu>
2017-06-17 16:40:46 -0700

    not fin h2

--- State.cpp
+++ State.cpp
@@ -23,6 +23,14 @@ void State::SetHeuristicValue(int xHValue) {
     mHeuristicValue = xHValue;
 }
 
+
+//===CalulateHeuristicOne==========================
+// Calulate Heruistic value base on the tile out of
+//	position in the board
+//	Compare current to the winning state
+// Return: 	Heruistic Value:
+//	** this does not store the cal value
+//==================================================
 int State::CalulateHeuristicOne(){
 	int count = 0;
 	if (mBoardState.GetBoard()[0] != mBoardState.GetWinBoard()[0]) count++;
@@ -34,5 +42,64 @@ int State::CalulateHeuristicOne(){
 	if (mBoardState.GetBoard()[6] != mBoardState.GetWinBoard()[6]) count++;
 	if (mBoardState.GetBoard()[7] != mBoardState.GetWinBoard()[7]) count++;
 	if (mBoardState.GetBoard()[8] != mBoardState.GetWinBoard()[8]) count++;
+	return count;
+}
+
+
+//===CalulateHeuristicTwo==========================
+// Calulate Heruistic value base on how far the tile
+//	Is from the correct position
+// Return: 	Heruistic Value:
+//	** this does not store the cal value
+//==================================================
+int State::CalulateHeuristicTwo(){
+	int count = 0;
+	int temp;
+
+	for(index = 0; index < Board_Size; index++)
+	{
+		//index store correct tile number
+		//temp store the current tile the number is in
+		temp = mBoardState.FindCharInBoard(mBoardState.GetWinBoard()[index]);
+
+		if(temp != index){
+			if((temp - index) == 1){
+				if(mBoardState.IsMovable(RIGHT)) count++;
+				else count = count + 3;
+			}
+			else if (temp - index ==  -1){
+				if(mBoardState.IsMovable(LEFT)) count++;
+				else count = count + 3;
+			}
+			else if(abs(temp - index) == 2){
+				count = count + 2;
+			}
+			else if(abs(temp - index) == 3){
+				count++;
+			}
+			else if(abs(temp - index) == 4){
+				count = count + 2;
+			}
+			else if(abs(temp - index) == 5){
+				count = count + 3;
+			}
+			else if(abs(temp - index) == 8){
+				count = count + 4;
+			}
+			else if(abs(temp - index) == 4){
+				count = count + 2;
+			}
+
+
+
+		}
+
+
+
+
+	}
+
+
+
 	return count;
 }
