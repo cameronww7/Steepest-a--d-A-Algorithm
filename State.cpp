7cf302a758e7daf0265f5a0e3702353d3c4aa49f
cam <cam@localhost.localdomain>
2017-06-20 11:32:14 -0700

    Function Documentation of State class

--- State.cpp
+++ State.cpp
@@ -25,23 +25,58 @@ State::State(EightGame xEGBoard) {
     mOldMove 		= 0;
 }
 
+//==================SetBoard========================
+// Takes in a passed in board and assigns it to a
+// class member variable.
+//-------------------------------------------------
+// In    : EightGame Board
+// Return: 	Nothing
+//==================================================
 void State::SetBoard(EightGame xEGBoard) {
     mCurrentBoard.SetBoard(xEGBoard.GetBoard());
     mCurrentBoard.SetWinBoard(xEGBoard.GetWinBoard());
 }
 
+//==================SetHeuristicValue===============
+// Takes in a Heuristic value and then assigns it
+// to a member variable.
+//-------------------------------------------------
+// In    : a Heuristic value
+// Return: 	Nothing
+//==================================================
 void State::SetHeuristicValue(int xHValue) {
     mHeuristicValue = xHValue;
 }
 
+//==================DisplayState===================
+// Displays the current contents of the member
+// variable board.
+//-------------------------------------------------
+// In    :  Nothing
+// Return: 	Nothing
+//==================================================
 void State::DisplayState() {
 	mCurrentBoard.DisplayBoard();
 }
 
+//================="operator="======================
+// A overload function of the = operator.
+//-------------------------------------------------
+// In    : EightGame Board
+// Return: 	Nothing
+//==================================================
 State State::operator=(const State & xRHS) {
 	return xRHS;
 }
 
+//================="operator=="=====================
+// A overload function of the == operator.
+// Will take in a state to be checked if it is
+// the same state as its being compared to.
+//-------------------------------------------------
+// In    :  State to be compared
+// Return: 	If the states are the same or not.
+//==================================================
 bool State::operator==(State & xRHS) {
 	bool returnBool = true;
 	for(int index = 0; index < BOARD_SIZE; index++) {
@@ -53,6 +88,14 @@ bool State::operator==(State & xRHS) {
 	return returnBool;
 }
 
+//==================operator>=======================
+// A overload function of the > operator.
+// Will take in a state to be compared if it is
+// greater than the state being compared too.
+//-------------------------------------------------
+// In    :  State to be compared
+// Return: 	If the state is greater
+//==================================================
 bool State::operator>(State & xRHS) {
 	bool returnBool = false;
 	if (this->GetHeuristicValue() > xRHS.GetHeuristicValue()) {
